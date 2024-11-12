4d38c46ea59667d764de3f0442148ba79585ef3a
henriettetuombe <h.tuombe@alustudent.com>
2024-11-12 08:16:38 +0200

    Initial commit

--- portfolio.js
+++ portfolio.js
@@ -1,202 +1,131 @@
 const menu = document.getElementById("menu");
 const ul = document.querySelector(".display-menu");
-const blogs = JSON.parse(localStorage.getItem("blogContent"))
+
+// Toggle the menu open/close on click
 menu.addEventListener("click", (e) => {
-    if (menu.innerHTML === '<i class="fa-solid fa-bars"></i>') {
-        e.preventDefault();
-        ul.classList.toggle("show-menu");
-
-        menu.innerHTML = '<i class="fa-solid fa-xmark"></i>';
-    }
-    if (menu.innerHTML === '<i class="fa-solid fa-xmark"></i>') {
-        menu.addEventListener("click", (e) => {
-            ul.classList.remove("display-menu")
-            menu.innerHTML = '<i class="fa-solid fa-bars"></i>';
-
-        });
-    }
+    e.preventDefault();
+    ul.classList.toggle("show-menu");
+    menu.innerHTML = ul.classList.contains("show-menu") 
+        ? '<i class="fa-solid fa-xmark"></i>' 
+        : '<i class="fa-solid fa-bars"></i>';
 });
 
+// Display blogs dynamically based on localStorage data
+const blogs = JSON.parse(localStorage.getItem("blogContent")) || [];
+const blogContainers = document.querySelectorAll(".container-blog");
 
-
-//working with blogs and display it in the portfolio
-
-// const blogs = JSON.parse(localStorage.getItem("blogContent"))
-// const blogContainer = document.querySelector(".container-blog")
-// let element = "";
-// blogs.forEach((rec) => {
-//     element += `
-//     <div>
-//         <h1>${rec.title}</h1>
-//         <p>${rec.description}</p>
-//         <div id="read-more">
-//             <a href="" style="margin-left: 30px;">Read more</a>
-//             <a href=""><i class="fa-solid fa-circle-right"></i></a>
-//         </div>
-//     </div>
-    
-//     `
-// })
-// blogContainer.innerHTML = element;
-
-
-
-    
-if(blogs[0] && blogs[1]){
-    let upDatedBlog = [];
-    let firstBlog = blogs[0];
-    let secondBlog = blogs[1];
-    upDatedBlog.push(firstBlog,secondBlog)
-    console.log(upDatedBlog)
-
-    const blogContainer = document.querySelector(".container-blog.blog1");
-    let element = "";
-    upDatedBlog.forEach((rec) => {
+if (blogs.length > 0) {
+    blogContainers.forEach((container, index) => {
+        let element = "";
+        const start = index * 2;
+        const end = start + 2;
+        blogs.slice(start, end).forEach((rec) => {
             element += `
-            <div>
-                <h1>${rec.title}</h1>
-                <p>${rec.description}</p>
-                <div id="read-more">
-                    <a href="" style="margin-left: 30px;">Read more</a>
-                    <a href=""><i class="fa-solid fa-circle-right"></i></a>
+                <div>
+                    <h1>${rec.title}</h1>
+                    <p>${rec.description}</p>
+                    <div id="read-more">
+                        <a href="#" style="margin-left: 30px;">Read more</a>
+                        <a href="#"><i class="fa-solid fa-circle-right"></i></a>
+                    </div>
                 </div>
-            </div>
-            
-            `
-    })
-    blogContainer.innerHTML = element;
-} 
-if (blogs[2] && blogs[3]){
-    let upDatedBlog1 = [];
-    let thirdBlog = blogs[2];
-    let forthBlog = blogs[3];
-    upDatedBlog1.push(thirdBlog, forthBlog);
-    console.log("hello",upDatedBlog1)
-    const blogContainer = document.querySelector(".container-blog.blog2");
-    let element = "";
-    upDatedBlog1.forEach((rec) => {
-            element += `
-            <div>
-                <h1>${rec.title}</h1>
-                <p>${rec.description}</p>
-                <div id="read-more">
-                    <a href="" style="margin-left: 30px;">Read more</a>
-                    <a href=""><i class="fa-solid fa-circle-right"></i></a>
-                </div>
-            </div>
-            
-            `
-    })
-    blogContainer.innerHTML = element;
-
+            `;
+        });
+        container.innerHTML = element;
+    });
 }
 
-if(blogs.length > 4){
-    const blogContainer = document.querySelector(".container-blog.blog2");
-    let element = "";
-    upDatedBlog1.forEach((rec) => {
-            element += `
-            <div>
-                <h1>${rec.title}</h1>
-                <p>${rec.description}</p>
-                <div id="read-more">
-                    <a href="" style="margin-left: 30px;">Read more</a>
-                    <a href=""><i class="fa-solid fa-circle-right"></i></a>
-                </div>
-            </div>
-            
-            `
-    })
-    blogContainer.innerHTML = element;
-}
-    
-//end of working with blogs
-
-
+// Form validation and submission
 const fullName = document.getElementById('full-name');
 const email = document.getElementById('email');
-const messages = document.getElementById('message')
-const errorMessage = document.getElementById('errorProvidedMessage')
-//adding some padding from the form input
+const messages = document.getElementById('message');
+const errorMessage = document.getElementById('errorProvidedMessage');
+
+// Adding padding to form inputs
 fullName.style.paddingLeft = '20px';
 email.style.paddingLeft = '20px';
 messages.style.padding = '30px';
-//end of padding
+
 document.getElementById('form').addEventListener('submit', (e) => {
-    e.preventDefault()
-    errors = []
-    if(fullName.value.trim() === ""){
+    e.preventDefault();
+    const errors = [];
+
+    // Validate full name
+    if (fullName.value.trim() === "") {
         fullName.style.border = '1px solid red';
-        errors.push("please provide Your full name");
+        errors.push("Please provide your full name");
     } else {
         fullName.style.border = 'black';
     }
-    if (!(email.value.includes("@"))){
+
+    // Validate email
+    if (!email.value.includes("@")) {
         email.style.border = '1px solid red';
-        errors.push("invalid email")
+        errors.push("Invalid email");
     } else {
-        fullName.style.border = 'black';
+        email.style.border = 'black';
     }
-    if (email.value.trim() === ""){
+
+    if (email.value.trim() === "") {
         email.style.border = '1px solid red';
-        errors.push("provide an email")
+        errors.push("Please provide an email");
     }
-    
-    
-    if (errors.length > 0){
-        errorMessage.textContent = errors.join(", ")
-        errorMessage.style.color='red';
+
+    if (errors.length > 0) {
+        errorMessage.textContent = errors.join(", ");
+        errorMessage.style.color = 'red';
         errorMessage.style.fontSize = '15px';
         errorMessage.style.marginBottom = '10px';
     } else {
-        // window.location.href = "loading.html"
-        storingMessage();
+        storeMessage();
         fullName.value = '';
         email.value = '';
         messages.value = '';
-        Notification.requestPermission()
-            .then((permis) => {
-                if (permis === 'granted') {
-                    const notification = new Notification('New Message', {
-                        body: " New Inquiry from contact me",
-                        icon: "images/Myimage.jpg"
-                    })
-                }
-            })
-        
+        errorMessage.textContent = "";
+        notifyUser("New Inquiry from Contact Form");
     }
-})
-function storingMessage(){
+});
+
+// Store messages in localStorage
+function storeMessage() {
     const messageObj = JSON.parse(localStorage.getItem('contactMessage')) || [];
-    const inputName = document.getElementById('full-name').value;
-    const inputEmail = document.getElementById('email').value;
-    const inputMessage = document.getElementById('message').value;
     const newMessage = {
-        id:messageObj.length+1,
-        name:inputName,
-        email:inputEmail,
-        message:inputMessage
-    }
+        id: messageObj.length + 1,
+        name: fullName.value,
+        email: email.value,
+        message: messages.value
+    };
     messageObj.push(newMessage);
-    localStorage.setItem('contactMessage', JSON.stringify(messageObj))
-    console.log(JSON.parse(localStorage.getItem('contactMessage')))
+    localStorage.setItem('contactMessage', JSON.stringify(messageObj));
+    console.log("Stored messages:", messageObj);
 }
 
+// Send a notification to the user
+function notifyUser(message) {
+    Notification.requestPermission().then((permission) => {
+        if (permission === 'granted') {
+            new Notification('New Message', {
+                body: message,
+                icon: "images/Myimage.jpg"
+            });
+        }
+    });
+}
 
-function displayProjectsInPage(){
-    const projectstored = JSON.parse(localStorage.getItem('projects') || []);
-    const projectContainer = document.querySelector(".projects-section-container"); 
+// Display projects from localStorage
+function displayProjectsInPage() {
+    const projectStored = JSON.parse(localStorage.getItem('projects')) || [];
+    const projectContainer = document.querySelector(".projects-section-container");
     let elem = "";
-    projectstored.forEach((pro) => {
+    projectStored.forEach((project) => {
         elem += `
-        <div class="project-div3 single-project">
-            <h1>${pro.title}</h1>
-            <p>${pro.description}</p>
-            <button>DEMO</button>
-        </div>
-        `
-    })
+            <div class="project-div3 single-project">
+                <h1>${project.title}</h1>
+                <p>${project.description}</p>
+                <button>DEMO</button>
+            </div>
+        `;
+    });
     projectContainer.innerHTML = elem;
-
 }
-displayProjectsInPage()
+displayProjectsInPage();
